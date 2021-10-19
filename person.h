97116c330c538b5f01db6c2588561b2b375393b8
turn.exe <80058678+typeplusplus@users.noreply.github.com>
2021-10-18 18:42:35 -0700

    Update person.h

--- person.h
+++ person.h
@@ -13,8 +13,24 @@ class Person
     Person(string pname, string pgender);
 
     //Mutators
+/*
+setName member function: sets name to name
+@param - name
+@return - None
+*/
     void setName(string name);
+/*
+setGender member function: sets gender to pgender
+@param - pgender
+@return - None
+*/
     void setGender(string pgender);
+/*
+display member function: prints out information 
+for name and gender.
+@param - name
+@return - None
+*/
     void display() const;
 
   private:
