8602a00cc40322cf1dccc55ef5cddaa955906109
turn.exe <80058678+typeplusplus@users.noreply.github.com>
2021-10-18 18:42:04 -0700

    Update instructor.h

--- instructor.h
+++ instructor.h
@@ -13,7 +13,19 @@ class Instructor : public Person
         Instructor(string name, string gender, double amount);
 
         //Mutator
+        /*
+        setSalary member function: Sets salary equal amount value
+        @param - amount: parameter passed containing how much the persons salary is
+        @return - None
+        */
         void setSalary(double amount);
+        
+        //Accessor
+        /*
+        display member function: prints the member data information
+        @param - None
+        @return - None
+        */
         void display() const;
     
     private:
